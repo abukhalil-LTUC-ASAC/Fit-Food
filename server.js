9e41a8556e2f5a3809dcdddfd9a9f79165233410
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-08-15 00:43:16 +0300

    creat constructor

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 // -------------------------------- DECLARE VARIABLES --------------------------------
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config('.env');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config(".env");
+const expressLayouts = require("express-ejs-layouts");
 // const pg = require('pg');
 // var methodOverride = require('method-override');
 
@@ -14,6 +14,12 @@ const app = express();
 // Declare a port
 const PORT = process.env.PORT || 3000;
 
+// Declare a app id for edmam
+const APP_ID = process.env.APP_ID;
+
+// Declare a app id for edmam
+const APP_KEY = process.env.APP_KEY;
+
 // using layouts
 app.use(expressLayouts);
 
@@ -21,39 +27,79 @@ app.use(expressLayouts);
 app.use(cors());
 
 // Use super agent
-const superagent = require('superagent');
+const superagent = require("superagent");
 
 // view engine setup
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //setup public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //set the encode for post body request
 app.use(express.urlencoded({ extended: true }));
 
 // connect to the server
 app.listen(PORT, () => {
-  console.log('I am listening to port: ', PORT);
+  console.log("I am listening to port: ", PORT);
 });
 
 // -------------------------------- ROUTES --------------------------------
 
 // Home route
-app.get('/', homeHandler);
+app.get("/", homeHandler);
 
 // get search
 
-app.get('/search',searchHandler)
+app.get("/search", searchHandler);
 
 // -------------------------------- CALLBACK FUNCTIONS --------------------------------
 
 //home
 function homeHandler(req, res) {
-  res.render('index');
+  res.render("index");
 }
 
 //search
-function searchHandler(req, res){
-  console.log(req.body);
+async function searchHandler(req, res) {
+  let ingredients = req.query.searchFood;
+  let recipes = await getRecipes(ingredients);
+  res.send(recipes);
+}
+
+// -------------------------------- API FUNCTIONS --------------------------------
+
+function getRecipes(ingredients) {
+  let url = "https://api.edamam.com/search";
+  let queryParams = {
+    q: ingredients,
+    app_id: APP_ID,
+    app_key: APP_KEY,
+  };
+  let result = superagent
+    .get(url)
+    .query(queryParams)
+    .then((res) => {
+      return res.body.hits.map((e) => {
+        return new Recipe(e);
+      });
+    })
+    .catch((error) => {
+      return {
+        status: error.status,
+        message: error.response.text,
+      };
+    });
+  return result;
+}
+
+// -------------------------------- CONSTRUCTORS --------------------------------
+
+function Recipe(data) {
+  this.title = data.recipe.label;
+  this.image = data.recipe.image;
+  this.ingredients = data.recipe.ingredientLines;
+  this.totalCalories = Math.round(data.recipe.calories);
+  this.servings = data.recipe.yield;
+  this.instructions_url = data.recipe.url;
+  this.calPerServ = Math.round(this.totalCalories / this.servings);
 }
