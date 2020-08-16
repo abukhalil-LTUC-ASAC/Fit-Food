f6e3725a96dd12ad61fa700a7fdedd025966245c
roukiaSalahi <roqaia.sa@hotmail.com>
2020-08-16 11:31:36 +0300

    done

--- data/meals.sql
+++ data/meals.sql
@@ -1,8 +1,14 @@
 DROP TABLE IF EXISTS meals ;
-CREATE TABLE meals (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    totalCalories VARCHAR(255),
-    ingredients VARCHAR(255),
-    date VARCHAR(255)
+DROP TABLE IF EXISTS recipes;
+CREATE TABLE recipes (
+  id SERIAL PRIMARY KEY,
+  uri VARCHAR ( 255 ),
+  title VARCHAR(255),
+  image VARCHAR(255),
+  ingredients text[],
+  totalCalories VARCHAR(255),
+  servings VARCHAR(255),
+  instructions_url VARCHAR(255),
+  calPerServ VARCHAR(255),
+  date VARCHAR(255)
 );
