0e013ea0e43eddeb764903683a2fbfb09579830b
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-08-17 22:02:09 +0300

    addning error 404 page

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ require("dotenv").config(".env");
 const expressLayouts = require("express-ejs-layouts");
 const pg = require("pg");
 
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const client = new pg.Client(process.env.DATABASE_URL);
-
 
 // initialize the server
 const app = express();
@@ -35,7 +34,6 @@ app.use(cors());
 // Use super agent
 const superagent = require("superagent");
 
-
 // view engine setup
 app.set("view engine", "ejs");
 
@@ -60,7 +58,6 @@ client.connect().then(() => {
 // Home route
 app.get("/", homeHandler);
 
-
 // get search
 app.get("/search", searchHandler);
 
@@ -82,22 +79,14 @@ app.post("/dataIng", renderIngredients);
 // get recipe by uri
 app.get("/recipeDetails/", recipeDetailsHandler);
 
-//Error route 
-app.all('*' , errorHandler);
-
 // all other routes
-// app.get("*", errorHandler);
+app.all("*", errorHandler);
 
 // -------------------------------- CALLBACK FUNCTIONS --------------------------------
 
 //home
 function homeHandler(req, res) {
   res.render("index");
-}
-
-//error
-function errorHandler(req,res) {
-  res.status(404).render('pages/error');
 }
 
 //search
@@ -109,7 +98,6 @@ async function searchHandler(req, res) {
   let health = req.query.health;
   let excluded = req.query.excluded;
   let ingr = req.query.ingr;
-
   let recipes = await getRecipes(
     ingredients,
     from,
@@ -120,19 +108,11 @@ async function searchHandler(req, res) {
     ingr
   );
 
-  res.render("pages/recipeResult", {
-    recipes: recipes
-  });
-  // if (recipes.status === 200) {
-  //   res.render("pages/recipeResult", {
-  //     recipes: recipes,
-  //   });
-  // } else {
-  //   res.render('pages/error' , {
-  //     error : recipes,
-  //   })
-  //   console.log(recipes);
-  // }
+  if (recipes) {
+    res.render("pages/recipeResult", {
+      recipes: recipes,
+    });
+  }
 }
 
 //fav
@@ -153,8 +133,7 @@ async function addFav(req, res) {
 async function deleteFav(req, res) {
   const recipeId = req.params.id;
   let result = await deleteRecipeDB(recipeId);
-  res.redirect('/fav');
-
+  res.redirect("/fav");
 }
 
 //calculate
@@ -162,22 +141,21 @@ function calculateCalories(req, res) {
   res.render("pages/calorieCalculator");
 }
 
-// render result 
+// render result
 async function renderIngredients(req, res) {
   let length = Math.floor(Object.keys(req.body).length / 3) - 1;
-  // let stringArray = [];
-
   for (var i = 0; i <= length; i++) {
-    let stringName = 'searchIngredient' + i;
-    let stringAmount = 'ingredientAmount' + i;
-    let stringMeasure = 'ingredientMeasure' + i;
-    let allString = req.body[stringName] + ' ' + req.body[stringAmount] + ' ' + req.body[stringMeasure];
-    // stringArray.push(allString);
-    // console.log(stringArray);
-
+    let stringName = "searchIngredient" + i;
+    let stringAmount = "ingredientAmount" + i;
+    let stringMeasure = "ingredientMeasure" + i;
+    let allString =
+      req.body[stringName] +
+      " " +
+      req.body[stringAmount] +
+      " " +
+      req.body[stringMeasure];
     let nutrition = await getNutrition(allString);
     console.log(nutrition);
-
   }
 }
 
@@ -185,15 +163,20 @@ async function renderIngredients(req, res) {
 async function recipeDetailsHandler(req, res) {
   let uri = req.query.uri;
   let recipe = await getRecipeByURI(uri);
-  res.render('pages/recipeDetail', { recipe: recipe });
+  res.render("pages/recipeDetail", { recipe: recipe });
 }
 
+//error
+function errorHandler(req, res) {
+  res.status(404).render("pages/error", {
+    message: "page not found !"
+  });
+}
 
 // -------------------------------- API FUNCTIONS --------------------------------
 
 //search recipe API
 function getRecipes(ingredients, from, to, diet, health, excluded, ingr) {
-  console.log('ingr: ',ingr);
   let url = "https://api.edamam.com/search";
   let queryParams = {
     q: ingredients,
@@ -204,33 +187,25 @@ function getRecipes(ingredients, from, to, diet, health, excluded, ingr) {
     diet: diet,
     health: health,
     excluded: excluded,
-    ingr: ingr
+    ingr: ingr,
   };
 
   if (excluded.length === 0) {
     delete queryParams.excluded;
-  };
+  }
 
   if (ingr.length === 0) {
     delete queryParams.ingr;
-  };
+  }
   let result = superagent
     .get(url)
     .query(queryParams)
     .then((res) => {
-      console.log(res.body);
       return res.body.hits.map((e) => {
         return new Recipe(e);
       });
     })
     .catch((error) => {
-      return handleError(error, res);
-//       console.log(error);
-
-//       return {
-//         status: error.status,
-//         message: error.response.text,
-//       };
     });
   return result;
 }
@@ -241,7 +216,7 @@ function getNutrition(string) {
   let queryParams = {
     app_id: APP_ID_NUT,
     app_key: APP_KEY_NUT,
-    ingr: string
+    ingr: string,
   };
   let result = superagent
     .get(url)
@@ -251,8 +226,7 @@ function getNutrition(string) {
       return new Nutrients(res.body);
     })
     .catch((error) => {
-      return handleError(error, res);
-    })
+    });
   console.log(result);
   return result;
 }
@@ -273,15 +247,8 @@ function getRecipeByURI(uri) {
       return new Recipe({ recipe: res.body[0] });
     })
     .catch((error) => {
-      return handleError(error, res);
-    })
+    });
   return result;
-}
-
-//error function 
-function handleError(error, res) {
-  console.error(error);
-  res.render('pages/error', {error: error});
 }
 
 // -------------------------------- DATA FUNCTIONS --------------------------------
@@ -301,70 +268,57 @@ function saveRecipeDB(recipeInfo) {
     recipeInfo.image,
     recipeInfo.data,
   ];
-  return client.query(SQL, recipeArray)
+  return client
+    .query(SQL, recipeArray)
     .then((result) => {
-      console.log(result);
-      // return result.rows;
+      return result.rows;
     })
     .catch((error) => {
-      return handleError(error, res);
-    })
+    });
 }
 
 // Get recipe from database
 function getRecipeDB() {
   let SQL = "SELECT * FROM recipes";
-  return client.query(SQL)
+  return client
+    .query(SQL)
     .then((result) => {
-      console.log('result', result)
+      console.log("result", result);
       return { meals: result.rows };
     })
     .catch((error) => {
-      console.log('error', error)
-      return handleError(error, res);
-    })
+    });
 }
-
-getRecipeDB();
-// https://api.edamam.com/api/nutrition-data?app_id=14955312&
-// app_key=b051ae90212f813de4b95da243ad9e8a&ingr=10%20apple 
-///----- 1 ingredient at a time with measuring 1cups%20apple
-
-// https://api.edamam.com/search?q=chicken&excluded=citrus&excluded=salt
-// &excluded=kosher%20salt&app_id=a49852e9&app_key=1d096000e385b476818f554e3b06870d&
-// from=0&to=3&calories=591-722&health=alcohol-free 
-///----- Excluded concatenated multiple times
 
 // delete recipe from database
 function deleteRecipeDB(recipeId) {
   let SQL = `DELETE FROM recipes WHERE id=${recipeId}`;
-  return client.query(SQL)
-    .then(result => {
+  return client
+    .query(SQL)
+    .then((result) => {
       return { meals: result.rows };
     })
     .catch((error) => {
-      return handleError(error, res);
-    })
-  }
+    });
+}
 
+// -------------------------------- CONSTRUCTORS --------------------------------
+function Recipe(data) {
+  this.uri = encodeURIComponent(data.recipe.uri);
+  this.title = data.recipe.label;
+  this.image = data.recipe.image;
+  this.ingredients = data.recipe.ingredientLines;
+  this.totalCalories = Math.round(data.recipe.calories);
+  this.servings = data.recipe.yield;
+  this.instructions_url = data.recipe.url;
+  this.calPerServ = Math.round(this.totalCalories / this.servings);
+}
 
-  // -------------------------------- CONSTRUCTORS --------------------------------
-  function Recipe(data) {
-    this.uri = encodeURIComponent(data.recipe.uri);
-    this.title = data.recipe.label;
-    this.image = data.recipe.image;
-    this.ingredients = data.recipe.ingredientLines;
-    this.totalCalories = Math.round(data.recipe.calories);
-    this.servings = data.recipe.yield;
-    this.instructions_url = data.recipe.url;
-    this.calPerServ = Math.round(this.totalCalories / this.servings);
-  }
-
-  function Nutrients(data) {
-    this.uri = encodeURIComponent(data.uri);
-    this.title = data.ingredients[0].parsed[0].food;
-    this.totalCalories = Math.round(data.calories);
-    this.weight = data.ingredients[0].parsed[0].weight;
-    this.id = data.ingredients[0].parsed[0].foodId;
-    // this.calPercentage = Math.round(totalCalories / maxCalories);
-  }
+function Nutrients(data) {
+  this.uri = encodeURIComponent(data.uri);
+  this.title = data.ingredients[0].parsed[0].food;
+  this.totalCalories = Math.round(data.calories);
+  this.weight = data.ingredients[0].parsed[0].weight;
+  this.id = data.ingredients[0].parsed[0].foodId;
+  // this.calPercentage = Math.round(totalCalories / maxCalories);
+}
