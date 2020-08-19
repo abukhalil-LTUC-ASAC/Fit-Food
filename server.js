10a862865702cac0b4bf5ada37ea22b19c63a621
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-08-19 10:21:36 +0300

    remove redirect on fav

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ async function addFav(req, res) {
   let localDate = dateNow.toLocaleDateString();
   recipeInfo.data = localDate;
   let result = await saveRecipeDB(recipeInfo);
-  res.redirect("/fav");
+  // res.redirect("/fav");
 }
 
 async function deleteFav(req, res) {
